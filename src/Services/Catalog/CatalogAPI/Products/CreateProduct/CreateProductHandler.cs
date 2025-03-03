e10e6286ae84ad095343e1dc7d4f31e2a40bdde9
Mohamed Hamada Gouda <m.gouda@tatweermea.com>
2025-03-04 00:27:13 +0400

    Add CQRS pattern for product creation
    Updated CatalogAPI.csproj to include MediatR.CommandQuery and container tools packages. Created CreateProductEndpoint class and defined CreateProductRequest and CreateProductResult records. Added CreateProductCommandHandler with unimplemented Handle method.

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    public record CreateProductRequest(string Name, decimal Price, List<string> Category, string ImageFile, string Description):IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductRequest, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
